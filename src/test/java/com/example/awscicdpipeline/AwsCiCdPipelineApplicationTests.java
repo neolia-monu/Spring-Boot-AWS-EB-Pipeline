daa9b60256b4d65da79b2ec18aa086894b3c2063
Monu Neolia <neoliamonu@gmail.com>
2022-08-14 16:16:27 +0530

    "initial commit"

---
+++ src/test/java/com/example/awscicdpipeline/AwsCiCdPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awscicdpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCiCdPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
